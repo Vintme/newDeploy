c291a7ef02a7ec080b287fc73ed3e1e74264e650
vintme <116039175+Vintme@users.noreply.github.com>
2025-03-17 19:43:47 -0300

    deploy2

--- vintme/src/app/page.tsx
+++ vintme/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         height={300}
         className="mb-6"
       />
-      <h1 className="text-2xl font-bold text-gray-800">Vintme Under Construction Last</h1>
+      <h1 className="text-2xl font-bold text-gray-800">Vintme Under Construction Last1</h1>
     </div>
   );
 }
