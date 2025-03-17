c9f55992f2c4470bb3641553bdda97c45be56157
vintme <116039175+Vintme@users.noreply.github.com>
2025-03-17 19:27:14 -0300

    firstcommit

---
+++ vintme/src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center p-6">
+      <Image
+        src="/images/construction.png"
+        alt="Under Construction"
+        width={300}
+        height={300}
+        className="mb-6"
+      />
+      <h1 className="text-2xl font-bold text-gray-800">Vintme Under Construction</h1>
+    </div>
+  );
+}
